8d782315f288d2b6f099e6c3258decdd4b57c7de
Aristóteles Pantaleão <62292967+AristotelesPantaleao@users.noreply.github.com>
2026-06-25 19:17:08 -0300

    Delete public/js/script.js

--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-function selecionar(elemento) {
-    document.querySelectorAll('.emote-box').forEach(el => {
-        el.classList.remove('selecionado');
-    });
-    elemento.classList.add('selecionado');
-}
